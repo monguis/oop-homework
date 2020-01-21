7eaa8b53426fe9dd88506be465340ec014691ce6
monguis <jccnnetwork@gmail.com>
2020-01-21 14:35:38 -0700

    passed all jest preset test

---
+++ CLI.js
@@ -0,0 +1,32 @@
+inquirer = require("inquirer");
+
+var fs = require('fs');
+
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "checkbox",
+    message: "What languages do you know?",
+    name: "stack",
+    choices: [
+      "HTML", 
+      "CSS", 
+      "JavaScript", 
+      "MySQL"
+    ]
+  },
+  {
+    type: "list",
+    message: "What is your preferred method of communication?",
+    name: "contact",
+    choices: [
+      "email",
+      "phone",
+      "telekinesis"
+    ]
+  }
+]).then(function(data) {}
