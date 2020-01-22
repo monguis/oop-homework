6fe15502e49d3f72614c97d5bc8f3a179bde51d9
monguis <jccnnetwork@gmail.com>
2020-01-21 21:07:23 -0700

    created basic app functionallity

--- generateHTML.js
+++ generateHTML.js
@@ -3,9 +3,7 @@ Manager = require("./lib/Manager");
 Intern = require("./lib/Intern");
 Engineer = require("./lib/engineer");
 const fs = require("fs");
-internIcon = `<i class="fas fa-user-graduate"></i>`;
-engineerIcon = `<i class="fas fa-glasses"></i>`;
-managerIcon = `<i class="fas fa-mug-hot"></i>`;
+
 
 
 generateFile = (team) => {
@@ -18,7 +16,8 @@ generateFile = (team) => {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>My Team</title>
+        <script src="https://kit.fontawesome.com/22ff5ec687.js" crossorigin="anonymous"></script>
+        <title>My Team</title>
 </head>
 
 <body>
@@ -30,14 +29,26 @@ generateFile = (team) => {
     <div class="container">
         <div class="row justify-content-center">`;
 
-
+    let lastLiStr = "";
     let cardCount = 0
     for (const member of team) {
+        let icon = "";
 
         if (cardCount === 3) {
             finalText += `</div>
             <div class="row justify-content-center">`
             cardCount = 0;
+        }
+
+        if (member.constructor.name === "Manager") {
+            icon = `<i class="fas fa-mug-hot"></i>`;
+            lastLiStr = "Office Number: " + member.getOfficeNumber();
+        } else if (member.constructor.name === "Engineer") {
+            icon = `<i class="fas fa-glasses"></i>`;
+            lastLiStr = "GitHub Profile: " + member.getGithub();
+        } else {
+            icon = `<i class="fas fa-user-graduate"></i>`;
+            lastLiStr = "School: " + member.getSchool();
         }
 
 
@@ -46,7 +57,7 @@ generateFile = (team) => {
     <div class="card text-white shadow rounded" style="max-width: 18rem;">
         <div class="card-header  bg-primary ">
             <h2>${member.getName()}</h2>
-            <h2>${member.constructor.name}</h2>
+            <h2>${icon}${member.constructor.name}</h2>
         </div>
         <div class="card-body bg-light">
             <ul class="list-group ">
@@ -54,14 +65,16 @@ generateFile = (team) => {
                ID: ${member.getId()}
                     </li>
                 <li class="list-group-item text-dark">Email: ${member.getEmail()}</li>
-                <li class="list-group-item text-dark"></li>
+                <li class="list-group-item text-dark">${lastLiStr}</li>
             </ul>
 
         </div>
     </div>
-</div>`;
+</div>
+`;
         cardCount++;
     }
+    if (cardCount !== 3) finalText += `</div>`
 
     finalText += `    </div>
 </body>
@@ -70,11 +83,11 @@ generateFile = (team) => {
 
     fs.writeFileSync("test.html", finalText, (err) => {
         if (err) throw err;
-        
+
         console.log("success!!")
     })
 
 }
 module.exports = {
     generateHTML: generateFile,
-  };
+};
